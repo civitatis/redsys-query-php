e389f4438f83cd889c745ea4a85a785fef4e485b
Omar Mohamad - El Hassan Lopesino <omar.lopesino@metadrop.net>
2019-05-14 15:25:13 +0200

    Coding style.

--- examples/get_transaction.php
+++ examples/get_transaction.php
@@ -7,9 +7,9 @@ use RedsysConsultasPHP\Client\Client;
 $url = 'https://sis-t.redsys.es:25443/apl02/services/SerClsWSConsulta';
 $client = new Client($url, 'Introduce your merchant password');
 
-$order = 'Introduce your order';
-$terminal = 'Introduce your terminal';
-$merchant_code = 'Introduce your merchant code';
+$order = 'Introduce Ds_Order';
+$terminal = 'Introduce Ds_terminal';
+$merchant_code = 'Introduce your Ds_merchantCode';
 $response = $client->getTransaction($order, $terminal, $merchant_code);
 
 print_r($response);
