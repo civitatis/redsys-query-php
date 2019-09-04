712fe72d214a97d1cad9fb0daeb0dee59d98d5c6
Ruben <resteban@civitatis.com>
2019-09-04 09:57:15 +0200

    Contemplar array de transacciones

--- src/Client/Client.php
+++ src/Client/Client.php
@@ -96,14 +96,14 @@ class Client extends GuzzleClient
      * @param $fecha_fin string Fecha de fin
      * @param $terminal string Número de terminal
      * @param $merchant_code string Código de comercio
-     * @return Transaction|null
+     * @return array Returned transactions array
      * @throws \Exception
      */
     public function getMonitorMasiva($fecha_inicio, $fecha_fin, $terminal, $merchant_code)
     {
         $payload = $this->buildPayload($this->requestGenerator->monitorMasiva($fecha_inicio, $fecha_fin, "7086YlQL", $terminal, $merchant_code));
         $response = $this->doRequest($payload);
-        return !empty($response) ? Transaction::fromXml($response) : NULL;
+        return !empty($response) ? Transaction::fromXmlArray($response) : NULL;
     }
 
     /**
--- src/Model/Transaction.php
+++ src/Model/Transaction.php
@@ -43,4 +43,41 @@ class Transaction extends BaseModel
 
         return $transaction;
     }
+
+    /**
+     * Generate array for transactions from xml response from webservice.
+     *
+     * @param \SimpleXMLElement $xml
+     *   XML.
+     *
+     * @return array
+     *   Transactions array.
+     *
+     * @throws \Exception
+     *   Error if xml does not have data.
+     */
+    public static function fromXmlArray(\SimpleXMLElement $xml)
+    {
+        $transaction_data = $xml->xpath('//Messages/Version/Message/Response');
+
+        $transaction = [];
+
+        if (empty($transaction_data)) {
+            // @TODO: custom exception!
+            throw new \Exception('There is no transaction data in response');
+        }
+
+        foreach ($transaction_data as $transaction_xml) {
+            $transaction_individual = new static();
+            foreach (RedsysFields::getList() as $field) {
+                $field_setter_method = 'set' . str_replace('_', '', $field);
+                if (method_exists($transaction_individual, $field_setter_method) && isset($transaction_xml->{$field})) {
+                    $transaction_individual->{$field_setter_method}((string) $transaction_xml->{$field});
+                    $transaction[] = $transaction_individual;
+                }
+            }
+        }
+
+        return $transaction;
+    }
 }
